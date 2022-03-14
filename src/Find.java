5aefaca19a2b03e232e1ddc07a12996db5d5c81a
MrAndrew <apantojaa2002@gmail.com>
2022-03-14 16:22:11 +0100

    Commit inicaial

---
+++ src/Find.java
@@ -0,0 +1,14 @@
+import java.util.List;
+
+
+public class Find {
+
+    private String text;
+    public Find(String text) {
+        this.text = text;
+    }
+    public boolean match(String pattern) {
+
+
+    }
+}
