97c9714510b3177f73839f9d42b1072177654f08
MrAndrew <apantojaa2002@gmail.com>
2022-03-14 16:59:17 +0100

    Primeros pasos

--- src/Find.java
+++ src/Find.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.List;
 
 
@@ -8,7 +9,26 @@ public class Find {
         this.text = text;
     }
     public boolean match(String pattern) {
+        String[] textChars = new String[this.text.length()];
+        textChars = text.split("");
+
+        //Creamos una array de strings que en cada posición contendrá un caracter.
+
+        System.out.println(Arrays.toString(textChars));
+
+        String[] patternarr = pattern.split("");
+        System.out.println(Arrays.toString(patternarr));
 
 
+        for (int i = 0; i < this.text.length(); i++) {
+            for (int j = 0; j < patternarr.length; j++) {
+                if (textChars[i].equals(patternarr[j])){
+                    if (textChars[i+1].equals(patternarr[j+1])){
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
     }
 }
