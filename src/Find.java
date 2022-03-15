977475218cac138e7d0eab6f2e04a7bee3519dab
Andres Pantoja <apantojaa2002@gmail.com>
2022-03-15 14:29:02 +0100

    Simple matches working, without test "Eo"

--- src/Find.java
+++ src/Find.java
@@ -2,7 +2,6 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 
-
 public class Find {
 
     private final String text;
@@ -14,34 +13,42 @@ public class Find {
     public boolean match(String pattern) {
         //Comprobamos si la strin esta vacia
 
-        if (this.text.length() == 0 || pattern.length() == 0){
+        if (this.text.length() == 0 || pattern.length() == 0) {
             return false;
         }
 
         int checked = 0;
         int index = 0;
+        boolean entrada = false;
+        boolean sigue = false;
         System.out.println(this.text.length());
-            for (int i = 0; i < this.text.length() ; i++) {
-                System.out.println(this.text.charAt(i));
-                System.out.println("Searching pattern: " + pattern.charAt(index));
-                    if (this.text.charAt(i) == pattern.charAt(index)){
-                        System.out.println("Match!");
-                        checked++;
+        for (int i = 0; i < this.text.length(); i++) {
+            System.out.println(this.text.charAt(i));
+            System.out.println("Searching pattern: " + pattern.charAt(index));
 
-                        if (pattern.length() > 1) index++;
-
-
-
-                        if (pattern.length() == checked){
-                            return true;
-                        }
-
-                    } else System.out.println("No mach");
-                }
+            if (this.text.charAt(i) == pattern.charAt(index)) {
+                entrada = true;
+                System.out.println("Match!");
+                checked++;
 
 
+                if (pattern.length() > 1) index++;
+
+                    if (pattern.length() == checked) {
+
+                        return true;
+                    }
+
+            }else {
+
+                System.out.println("No mach");
+                sigue = true;
+                entrada = false;
+
+            }
+
+        }
         return false;
     }
-
 }
 
