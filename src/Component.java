cc4d8fc3542bcd47a9f2cc2559ad9dc62ff0297a
Andres Pantoja <apantojaa2002@gmail.com>
2022-03-16 20:49:52 +0100

    Class patterns: Adding all special components

--- src/Component.java
+++ src/Component.java
@@ -6,7 +6,7 @@ public class Component{
         LBEGIN, //Si es comienzo de linea
         LINEEND, //Si es final de linea
         CONJUNTOCHARS, // Conjunto de chars entre corchetes [-----]
-        CLOSURE //
+        CLOSURE //* => 0 o más | + => 1 o más
     };
     TComponent tipo;
     char caracter;  // Caracter normal
--- src/Pattern.java
+++ src/Pattern.java
@@ -9,8 +9,28 @@ public class Pattern {
                 Component component = new Component();
                 component.tipo = Component.TComponent.QMARK;
                 components.addElement(component);
-
+            }else if (c == '%'){
+                Component component = new Component();
+                component.tipo = Component.TComponent.LBEGIN;
+                components.addElement(component);
+            }else if (c == '$'){
+                Component component = new Component();
+                component.tipo = Component.TComponent.LINEEND;
+                components.addElement(component);
+            }else if (c == '['){
+                Component component = new Component();
+                component.tipo = Component.TComponent.CONJUNTOCHARS;
+                components.addElement(component);
+            }else if (c == '+' || c == '*'){
+                Component component = new Component();
+                component.tipo = Component.TComponent.CLOSURE;
+                components.addElement(component);
+            }else {
+                Component component = new Component();
+                component.tipo = Component.TComponent.NORMALCHAR;
+                components.addElement(component);
             }
+
         }
     }
 
